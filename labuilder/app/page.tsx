783af43ca098b61c3954964bff72efba5ba53962
Luz Alba Posse <luzalbaposse@gmail.com>
2023-10-17 11:12:02 -0300

    starting again

--- labuilder/app/page.tsx
+++ labuilder/app/page.tsx
@@ -1,11 +1,8 @@
-import Image from 'next/image'
+import React from 'react';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen max-h-screen flex-col items-center justify-center p-24 bg-cover bg-center bg-white" style={{ backgroundImage: `url(Coming.png)` }}>
-     <div className="text-center">
-      <h2 className="font-['Syne', sans-serif] text-black font-regular text-2xl">Building...</h2>
-     </div>
-    </main>
-  )
+    
+  );
+
 }
